0db20990b1ef860f647a0b6de0c2a79b253e43b9
Zoddo <zoddo.ino@gmail.com>
2014-03-05 17:17:46 +0100

    [task/translation/french] Translate hooks/info_acp_hook_user.php

--- root/language/fr/mods/hooks/info_acp_hook_user.php
+++ root/language/fr/mods/hooks/info_acp_hook_user.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package language [English] phpBB API
+* @package language [Standard french] phpBB API
 * @version $Id: phpbb_api.php v1.0.0 16:55 02/11/2012 Geolim4 Exp $
 * @copyright (c) 2012 Geolim4.com  http://Geolim4.com
 * @bug/function request: http://geolim4.com/tracker.php
@@ -38,14 +38,14 @@ if (empty($lang) || !is_array($lang))
 //For add Color
 //
 $lang = array_merge($lang, array(
-	'ACP_API_MANAGE_HOOK_USER'		=> 'Information about user’s profile',
+	'ACP_API_MANAGE_HOOK_USER'		=> 'Informations sur le profile de l’utilisateur',
 ));
 
 $lang['API_FULL_TRANSLATED_METHOD']	+= array(
-		'user'			=> 'Get user data',
+		'user'			=> 'Obtenir les données de l’utilisateur',
 );
 $lang['API_TRANSLATED_METHOD']	+= array(
-		'user'			=> 'user',
+		'user'			=> 'utilisateur',
 );
 
 $lang['UCP_PHPBB_API_KNOWLEDGE_BASE_HOOKS'][] = array(
