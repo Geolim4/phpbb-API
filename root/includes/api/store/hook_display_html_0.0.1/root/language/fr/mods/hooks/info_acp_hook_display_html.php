88a4167748a76fa6eca92025756959b75c343867
Zoddo <zoddo.ino@gmail.com>
2014-03-08 17:16:38 +0100

    [task/translation/french] Update info_acp_hook_display_html.php

--- root/includes/api/store/hook_display_html_0.0.1/root/language/fr/mods/hooks/info_acp_hook_display_html.php
+++ root/includes/api/store/hook_display_html_0.0.1/root/language/fr/mods/hooks/info_acp_hook_display_html.php
@@ -5,7 +5,6 @@
 * @version $Id: phpbb_api.php v1.0.0 16:55 02/11/2012 Geolim4 Exp $
 * @copyright (c) 2012 Geolim4.com  http://Geolim4.com
 * @bug/function request: http://geolim4.com/tracker.php
-* @translator papicx 28/11/2013 14h50  version a papicx@phpbb-fr.com
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 */
 
@@ -34,12 +33,29 @@ if (empty($lang) || !is_array($lang))
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 // Some characters you may want to copy&paste:
-// ’ « » “ ” …
+// ’ » “ ” …
 // Use: <strong style="color:green">Texte</strong>',
 //For add Color
 //
 $lang = array_merge($lang, array(
-	'ACP_API_MANAGE_HOOK_DISPLAY_HTML' => 'Format de sortie HTML',
-	'ACP_API_OUTPUT_HTML_TITLE' => 'Interface API',
+	'ACP_API_MANAGE_HOOK_DISPLAY_HTML' => 'Sortie au format HTML',
+	'ACP_API_OUTPUT_HTML_TITLE' => 'Interface de la passerelle de l’API',
 	'ACP_API_OUTPUT_HTML_ROOT' => 'racine',
 ));
+
+$lang['UCP_PHPBB_API_KNOWLEDGE_BASE_HOOKS'][] = array(
+		//'method' => '',//Not translatable
+		//'a_'	=> true,//require the user to have at least an administrator key to view that paragraph!
+		0 => 'Récupérer les données au format HTML',
+		1 => 'Cette extension vous permettra de récupérer les données au format HTML (liste non-ordonnée).
+			[code=php] curl_setopt($handle, CURLOPT_POSTFIELDS, array(
+		"k" => "{KB_API_KEY}",
+		"e" => "{KB_USER_EMAIL}",
+		"a" => "topic",
+		"m" =>  false,
+		"t" => "topic_id",
+		"s" => "operator:LIKE",
+		"d" => "Bienvenue",
+		"o" => "html",
+));[/code]'
+);
