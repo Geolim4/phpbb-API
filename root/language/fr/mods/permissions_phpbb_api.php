2415d8fba0699bebe4e0c2f85ece5a23a4e4e134
Geolim4 <contact@geolim4.com>
2014-03-08 19:24:30 +0100

    Fixed head comment ;)

--- root/language/fr/mods/permissions_phpbb_api.php
+++ root/language/fr/mods/permissions_phpbb_api.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package language [English] phpBB API (phpBB Permission Set)
+* @package language [Standard french] phpBB API (phpBB Permission Set)
 ^>@version $Id: permissions_phpbb_api.php v0.0.1 18h35 03/08/2014 Zoddo Exp $
 * @copyright (c) 2012 Geolim4.com  http://Geolim4.com
 * @bug/function request: http://geolim4.com/tracker.php
